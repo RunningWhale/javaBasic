89fb87460bb2173b7c56f00f1e7ff9b60088e755
RunningWhale <2389444703@qq.com>
2020-12-13 11:17:16 +0800

    11

--- java每日一题/src/day09/Test01.java
+++
@@ -1,9 +0,0 @@
-package day09;
-
-public class Test01 {
-    public static void main(String[] args) {
-        String s="fmn";
-        String s1 = s.toUpperCase();
-        System.out.println(s1);
-    }
-}
--- java每日一题/src/day12/Test01.java
+++
@@ -1,8 +0,0 @@
-package day12;
-
-public abstract class Test01 {
-        public void method(){};
-        public abstract void method(int a);
-
-
-}
