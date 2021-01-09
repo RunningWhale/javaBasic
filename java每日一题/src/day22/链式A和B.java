ed47fe63d4e49a46ba5b6945ed6fb757227f1d71
RunningWhale <2389444703@qq.com>
2021-01-09 22:48:13 +0800

    每日一题

--- java每日一题/src/day22/链式A和B.java
+++ java每日一题/src/day22/链式A和B.java
@@ -14,6 +14,35 @@ class ListNode {
 
 class Plus {
     public ListNode plusAB(ListNode a, ListNode b) {
-       
+        ListNode curA = a;
+        int curAMul = 1;
+        int resultA = 0;
+        int resultB = 0;
+        int curBMul = 1;
+        ListNode curB = b;
+        while (curA != null) {
+            resultA += curA.val * curAMul;
+            curA = curA.next;
+            curAMul *= 10;
+        }
+        while (curB != null) {
+            resultB += curB.val * curBMul;
+            curB = curB.next;
+            curBMul *= 10;
+        }
+        int result = resultA + resultB;
+        String resultString = String.valueOf(result);
+        ListNode listNode = new ListNode(Integer.valueOf(resultString.substring(resultString.length() - 1)));
+        ListNode cur=listNode;
+        resultString = resultString.substring(0, resultString.length() - 1);
+        while (resultString.length()!=0){
+            ListNode curnode=new ListNode(Integer.valueOf(resultString.substring(resultString.length() - 1)));
+            cur.next=curnode;
+            cur=curnode;
+            resultString = resultString.substring(0, resultString.length() - 1);
+
+        }
+        return listNode;
+
     }
 }
---
+++ java每日一题/src/练习本/Substring.java
@@ -0,0 +1,11 @@
+package 练习本;
+
+public class Substring {
+    public static void main(String[] args) {
+        String s="123";
+        String substring = s.substring(0,0);
+        System.out.println(s);
+        System.out.println(substring.length());
+        System.out.println(substring);
+    }
+}
