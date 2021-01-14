14ea475dff664ceae30c3c42cb52a7e06e2dab3a
RunningWhale <2389444703@qq.com>
2021-01-14 23:41:43 +0800

    1

--- java每日一题/src/练习本/Demo01.java
+++ java每日一题/src/练习本/Demo01.java
@@ -1,11 +1,12 @@
 package 练习本;
 
+import java.math.BigInteger;
+
 public class Demo01 {
 
     public static void main(String[] args) {
-        System.out.println("The size of short is "+Short.BYTES+" bytes.");
-        System.out.println("The size of int is "+Integer.BYTES+" bytes.");
-        System.out.println("The size of long is "+Long.BYTES+" bytes.");
-        System.out.println("The size of long long is "+Long.BYTES+" bytes.");
+        BigInteger bigInteger=new BigInteger("3165757026");
+        String s=bigInteger.toString(2);
+        System.out.println(s);
     }
 }
